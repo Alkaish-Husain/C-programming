68fe8488fb71e984186f10d3a22920bbe5d1846e
Alkaish-Husain <alkaishhusain0@gmail.com>
2024-10-08 22:28:17 +0530

    Add files via upload

---
+++ My intro.c
@@ -0,0 +1,26 @@
+/* Expeiment No:- 1.1
+   Title   :-WAP to print your name, age, class, division and UIN on the screen.
+             Use escape sequences to properly format the output.
+   Name    :-Shaikh Alkaish Husain
+   Class   :-Computer Engineering
+   Division:-D
+   Roll No :-19
+   UIN     :-241P088 */
+#include <stdio.h>
+int main()
+{
+printf("Name:-\t\tShaikh Alkaish Husain Ahmad Husain\n");
+printf("Age:-\t\t18\n");
+printf("Class:-\t\tComputer Engineering\n");
+printf("Division:-\tD\n");
+printf("UIN:-\t\t241P088\n");
+return 0;
+}
+
+/*
+OUTPUT-
+Name:-          Shaikh Alkaish Husain Ahmad Husain
+Age:-           18
+Class:-         Computer Engineering
+Division:-      D
+UIN:-           241P088 */
